3b720963ba327bcf114333a762be64177eaa34c0
Pascal King <pmkgbwis@gmail.com>
2021-06-16 22:49:44 -0500

    Initial inventory slot setup

---
+++ Into Darkness/Assets/Scripts/Inventory/InventorySlotManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlotManager : MonoBehaviour
+{
+    [SerializeField] private InventoryNameTagManager nameTagManager;
+    [SerializeField] private InventoryStackTagManager stackTagManager;
+    [SerializeField] private ItemDisplayManager itemDisplayManager;
+    [SerializeField] private InventorySlotPanelManager panelManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Into Darkness/Assets/Scripts/Inventory/ItemDisplayManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplayManager : MonoBehaviour
+{
+    [SerializeField] private Image displaySlot;
+    [SerializeField] private Sprite displayActiveImage;
+    [SerializeField] private Sprite displayInactiveImage;
+    [SerializeField] private Image itemSlot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
